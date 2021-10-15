db3283b17a6a9c22e1f5e720ad656fbea765bab9
28niwa2108 <fle20-account@yahoo.co.jp>
2021-10-15 22:46:33 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/ }
+  validates :nickname, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birth_date, presence: true
+
 end
