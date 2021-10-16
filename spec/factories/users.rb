075913dab6890bc8602b154994cc745003c169ff
28niwa2108 <fle20-account@yahoo.co.jp>
2021-10-16 14:59:50 +0900

    userモックの定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6, mix_case: false) }
+    password_confirmation { password }
+    nickname              { Faker::Name.initials }
+    last_name             { Gimei.name.last.kanji }
+    first_name            { Gimei.name.first.hiragana }
+    last_name_kana        { Gimei.name.last.katakana }
+    first_name_kana       { Gimei.name.first.katakana }
+    birth_date            { Faker::Date.between(from: '1930-01-01', to: Date.today.prev_year(5)) }
   end
 end
