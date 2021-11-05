02da46931da23c485448ef30f126fcba67398e1b
28niwa2108 <fle20-account@yahoo.co.jp>
2021-11-05 21:22:41 +0900

    Rubocop指摘分の修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe '商品購入機能', type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id:@item.id)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
     sleep 0.1
   end
 
@@ -14,7 +14,7 @@ RSpec.describe '商品購入機能', type: :model do
     end
 
     it 'postal_codeが3桁ハイフン4桁の半角数値なら購入できる' do
-      @order_address.postal_code = "123-1234"
+      @order_address.postal_code = '123-1234'
       expect(@order_address).to be_valid
     end
 
@@ -52,18 +52,18 @@ RSpec.describe '商品購入機能', type: :model do
 
   context 'saveメソッドが成功するケース' do
     it 'Orderレコードが保存される' do
-      expect{@order_address.save}.to change{ Order.count }.by(1)
+      expect { @order_address.save }.to change { Order.count }.by(1)
     end
 
     it 'Addressレコードが保存される' do
       @order_address.save
-      expect{@order_address.save}.to change{ Address.count }.by(1)
+      expect { @order_address.save }.to change { Address.count }.by(1)
     end
   end
 
   context '商品購入ができないとき' do
     it 'postal_codeが空では購入できない' do
-      @order_address.postal_code = ""
+      @order_address.postal_code = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
     end
@@ -75,82 +75,82 @@ RSpec.describe '商品購入機能', type: :model do
     end
 
     it 'cityが空では購入できない' do
-      @order_address.city = ""
+      @order_address.city = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("City can't be blank")
     end
 
     it 'addressが空では購入できない' do
-      @order_address.address = ""
+      @order_address.address = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Address can't be blank")
     end
 
     it 'phone_numberが空では購入できない' do
-      @order_address.phone_number = ""
+      @order_address.phone_number = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
     end
 
     it 'user_idが存在しないと購入できない' do
-      @order_address.user_id = ""
+      @order_address.user_id = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("User can't be blank")
     end
 
     it 'item_idが存在しないと購入できない' do
-      @order_address.item_id = ""
+      @order_address.item_id = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Item can't be blank")
     end
 
     it 'tokenが空では購入できない' do
-      @order_address.token = ""
+      @order_address.token = ''
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Token can't be blank")
     end
 
     it 'postal_codeが3桁-ハイフン4桁(の半角数値)でないと購入できない' do
-      @order_address.postal_code = "1234-123"
+      @order_address.postal_code = '1234-123'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@order_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'postal_codeが(3桁ハイフン4桁の)半角数値でないと購入できない' do
-      @order_address.postal_code = "１２３-４５６"
+      @order_address.postal_code = '１２３-４５６'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      expect(@order_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
     it 'phone_numberが10桁未満の半角数値では購入できない' do
       @order_address.phone_number = 123456789
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone number is too short (minimum is 10 characters)")
+      expect(@order_address.errors.full_messages).to include('Phone number is too short (minimum is 10 characters)')
     end
 
     it 'phone_numberが11桁超過の半角数値では購入できない' do
       @order_address.phone_number = 123456789012
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      expect(@order_address.errors.full_messages).to include('Phone number is too long (maximum is 11 characters)')
     end
 
     it 'phone_numberが半角数値でなければ購入できない' do
       @order_address.phone_number = '123456789０'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Phone number is invalid. Input only number")
+      expect(@order_address.errors.full_messages).to include('Phone number is invalid. Input only number')
     end
   end
 
   context 'saveメソッドが失敗するケース' do
-      it 'いずれかの属性が欠損するとOrderレコードが保存されない' do
-        @order_address.user_id = ""
-        expect{ @order_address.save }.to change{ Order.count }.by(0)
-      end
+    it 'いずれかの属性が欠損するとOrderレコードが保存されない' do
+      @order_address.user_id = ''
+      expect { @order_address.save }.to change { Order.count }.by(0)
+    end
 
-      it 'order_idが発行されないと(Orderレコードの保存が成功しないと)Addressレコードが保存されない' do
-        @order_address.user_id = ""
-        expect{ @order_address.save }.to change{ Order.count }.by(0)
-        expect{ @order_address.save }.to change{ Address.count }.by(0)
-      end
+    it 'order_idが発行されないと(Orderレコードの保存が成功しないと)Addressレコードが保存されない' do
+      @order_address.user_id = ''
+      expect { @order_address.save }.to change { Order.count }.by(0)
+      expect { @order_address.save }.to change { Address.count }.by(0)
+    end
   end
 end
